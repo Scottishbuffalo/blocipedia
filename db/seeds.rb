396429620b9b756e9b2d8d8b16442de5736b3985
Mike MacAdam <mikemacadam87@gmail.com>
2016-12-19 10:55:16 -0800

    checkpoint 6 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+25.times do
+  User.create!(
+      username: Faker::Internet.user_name,
+      email:  Faker::Internet.email,
+      password: Faker::Internet.password,
+      confirmed_at: Faker::Time.between(10.days.ago, Date.today, :all)
+  )
+end
+
+25.times do
+  Wiki.create!(
+          title: Faker::Hacker.say_something_smart,
+          body: Faker::Lorem.paragraph,
+          private: false,
+          user_id: Faker::Number.between(1, 25)
+  )
+end
